3da3711e8a8141f47a53dab478fb206ec5b23d68
https://github.com/anamariahuamani/mi-primera-app.git <huamaniayquipaanamaria30@gmail.com>
2025-10-16 08:43:23 -0500

    actualizado

--- scripts.js
+++ scripts.js
@@ -1,27 +1,38 @@
 function mostrarMensaje(){
-    console.log("hola como estas")
+    console.log("Holaaaa como estas");
 }
-function capturatexto(){
-    // capturamos el valor de input
-    let input=document.querySelector
-    ("#nuevatarea")
-    if (input.value==""){
-        alert("el input debe contener un teto")
-        return
+function agregarTarea(){
+function capturaTexto(){
+    //capturamos el elemento input
+    let input=document.querySelector("#nuevaTarea");
+    if(input.value===""){
+        alert("El campo no puede estar vacio");
+        return;
     }
-    // capturamos el valor de input
-    let valorInput=input.value
-    // capturamos el elemento ul
-    let ul=document.createElement("li")
-    // asignamos como contenido del li el valor capturado en input
-    li.textContent=valorInput
+
+    //capturamos el valor del input
+    let valorInput=input.value;
+
+    //creamos un elemento li
+    let ul=document.querySelector("#listaTareas");
+
+    //creamos un elemento li
+    let li=document.createElement("li");
+
+    //le asignamos como contenido el valor capturado del input
+    li.textContent=valorInput;
     
-    // creamos el boton eliminar
-    let button=document.createElement
-    ("button")
-    button.textContent="eliminar"
-    button.onclick=function
-    li.appendChild(button)
-    // le agregamos como hijo el li al elemento padre ul
-    ul.appendChild(li)
+    //creamos el boton eliminar
+    let button=document.createElement("button");
+    button.textContent="Eliminar";
+    li.appendChild(button);
+    button.onclick=function(){
+        ul.removeChild(li);
+    }
+    // le agrego como hijo el li al elemento padre ul
+    ul.appendChild(li);
+    console.log();
+
+    document.querySelector("#nuevaTarea");
+    input.value="";
 }

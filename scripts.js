c41f996ff35fae72f58bd178243194a1eb2894c6
https://github.com/anamariahuamani/mi-primera-app.git <huamaniayquipaanamaria30@gmail.com>
2025-10-16 11:07:19 -0500

    actualizando mi tarea basico

--- scripts.js
+++ scripts.js
@@ -1,38 +1,36 @@
 function mostrarMensaje(){
-    console.log("Holaaaa como estas");
+    console.log("hola chistes ya no sufras ")
 }
-function agregarTarea(){
+
 function capturaTexto(){
-    //capturamos el elemento input
-    let input=document.querySelector("#nuevaTarea");
-    if(input.value===""){
-        alert("El campo no puede estar vacio");
-        return;
+    let input=document.querySelector("#nuevaTarea")
+    if (input.value===""){
+        alert("el input debe tener wtexto")
+        return
     }
-
-    //capturamos el valor del input
-    let valorInput=input.value;
+    //capturamos el valor de input
+    let valorInput=input.value
+//capturamos el elemento ul
+    let ul=document.querySelector("#listaTareas")
 
     //creamos un elemento li
-    let ul=document.querySelector("#listaTareas");
+    let li=document.createElement("li")
 
-    //creamos un elemento li
-    let li=document.createElement("li");
+    //le asignamos como contenido de li el valor capturado del input
+    li.textContent=valorInput
 
-    //le asignamos como contenido el valor capturado del input
-    li.textContent=valorInput;
-    
-    //creamos el boton eliminar
-    let button=document.createElement("button");
-    button.textContent="Eliminar";
-    li.appendChild(button);
-    button.onclick=function(){
-        ul.removeChild(li);
-    }
-    // le agrego como hijo el li al elemento padre ul
-    ul.appendChild(li);
-    console.log();
+    //creamos el boton eleminar
+    let button=document.createElement("button")
+    button.textContent="eliminar"
+    button.onclick=function(){li.remove()}
+    li.appendChild(button)
+    //le agremos como hijo el li al elemento padre ul
+    ul.appendChild(li)
+ 
+    //esta linea de codigo limpia mi input
+    document.querySelector("#nuevaTarea").value=""
 
-    document.querySelector("#nuevaTarea");
-    input.value="";
+    //esta linea hacer focus en el input, lo mantienen activo listo para poder ingresar otro texto
+    input.focus()
+
 }
